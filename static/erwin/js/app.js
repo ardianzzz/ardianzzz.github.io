c6cd7a5fcaa454bf1ddc9ee07af58a6fd7030cf9
braindance <braindance@oxymoron.(none)>
2014-07-01 16:08:13 +0700

    add erwin

---
+++ static/erwin/js/app.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+
+
+ onePageScroll(".main", {
+     sectionContainer: "section",
+     loop: true,
+     responsiveFallback: false
+   });
+
+
+
+
+
+
+})
